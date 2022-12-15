74f60fe9bce98c2bfbbd102cf38ebd4ef2e7b5a1
Shajadul <shajadulrahe@gmail.com>
2022-12-15 17:05:48 +0600

    Feat: Send card data to firebase

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 function App() {
   const toggle = useSelector((state) => state.toggle.toggle);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://redux-shopping-cart-d9ca0-default-rtdb.firebaseio.com/cart.json",
+      {
+        method: "PUT",
+        body: JSON.stringify({ cart }),
+      }
+    );
+  }, [cart]);
   console.log(toggle);
   return (
     <Layout>
